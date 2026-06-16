851e64a19467ca83d15813f431b0996a83fcc99b
Natali-23 <shtegman.nata@yandex.ru>
2026-06-16 09:56:01 +0300

    upgrade negative test TestBoxPage and TestBoxTest

--- src/test/java/Selenium/Test/TestBoxTest.java
+++ src/test/java/Selenium/Test/TestBoxTest.java
@@ -3,39 +3,47 @@ package Selenium.Test;
 import Selenium.BaseTest;
 import Selenium.page.TestBoxPage;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestBoxTest extends BaseTest {
+    private TestBoxPage testBoxPage;
 
+    @BeforeEach
+    public void initPage() {
+        testBoxPage = new TestBoxPage(getWebDriver());
+        getWebDriver().get("https://demoqa.com/text-box");
+    }
 
     @Test
-    public void testBox() {
-        getWebDriver().get("https://demoqa.com/text-box");
-
-        TestBoxPage testBoxPage = new TestBoxPage(getWebDriver());
+    public void positiveTestBox() {
         testBoxPage.FullName("Катя");
         testBoxPage.Email("qwerty@yandex.ru");
         testBoxPage.currentAddress("улица пупкина, дом залупкина");
         testBoxPage.permaentAddress("совпадает с нынешним адресом");
-        testBoxPage.button();
+        testBoxPage.submitButton();
 
-        // Проверка Имени
         Assertions.assertTrue(testBoxPage.getResultName().contains("Катя"),
                 "Имя не совпадает! Ожидали Катя, а получили: " + testBoxPage.getResultName());
-
-        // Проверка Email
         Assertions.assertTrue(testBoxPage.getResultEmail().contains("qwerty@yandex.ru"));
-
-        // Проверка Current Address (самое проблемное место)
         Assertions.assertTrue(testBoxPage.getResultCurrentAddress().contains("улица пупкина, дом залупкина"),
                 "Адрес не найден в строке: " + testBoxPage.getResultCurrentAddress());
-
-        // Проверка Permanent Address (исправил опечатку в слове Permanent)
         Assertions.assertTrue(testBoxPage.getResultPermanentAddress().contains("совпадает с нынешним адресом"));
-
     }
+@Test
+   public void shouldShowErrorForInvalidEmail(){
+        testBoxPage.Email("testmail.ru");
+    testBoxPage.submitButton();
+    System.out.println(testBoxPage.emailHasError());
+    Assertions.assertTrue(testBoxPage.emailHasError(),"Поле Email должно быть подсвечено как ошибочное");
 }
+
+@Test
+    public void shouldShowEmptyEmail(){
+    testBoxPage.Email("");
+    testBoxPage.submitButton();
+    System.out.println(testBoxPage.emailHasError());
+    Assertions.assertFalse(testBoxPage.emailHasError(),"Поле Email должно быть пустым");
+}
+}
+
--- src/test/java/Selenium/page/TestBoxPage.java
+++ src/test/java/Selenium/page/TestBoxPage.java
@@ -1,12 +1,8 @@
 package Selenium.page;
 
-import Selenium.BaseTest;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestBoxPage {
     private WebDriver webDriver;
@@ -17,41 +13,43 @@ public class TestBoxPage {
     private By permanentAddressTestBox = By.xpath("//textarea[@id = 'permanentAddress']");
     private By submitTestBox = By.xpath("//button[@id='submit']");
 
+
     //локаторы вывода после нажатия кнопки
     private By findNameByLocatorTestBox = By.xpath("//p[@id='name']");
     private By findEmailByLocatorTestBox = By.xpath("//p[@id ='email']");
     private By findCurrentAddressByLocatorTestBox = By.xpath("//p[@id='currentAddress']");
     private By findPermanentAddressByLocatorTestBox = By.xpath("//p[@id='permanentAddress']");
+    private By userEmail = By.id("userEmail");
 
     public TestBoxPage(WebDriver webDriver) {
         this.webDriver = webDriver;
     }
 
-    public void FullName(String name){
-       WebElement fullname = webDriver.findElement(fullNameLocatorTestBox);
-       fullname.clear();
+    public void FullName(String name) {
+        WebElement fullname = webDriver.findElement(fullNameLocatorTestBox);
+        fullname.clear();
         fullname.sendKeys(name);
     }
 
-    public void Email(String e){
+    public void Email(String e) {
         WebElement email = webDriver.findElement(emailLocatorTestBox);
         email.clear();
         email.sendKeys(e);
     }
 
-    public void currentAddress(String address){
+    public void currentAddress(String address) {
         WebElement curAddress = webDriver.findElement(currentAddressTestBox);
         curAddress.clear();
         curAddress.sendKeys(address);
     }
 
-    public void permaentAddress(String address){
+    public void permaentAddress(String address) {
         WebElement permanAddress = webDriver.findElement(permanentAddressTestBox);
         permanAddress.clear();
         permanAddress.sendKeys(address);
     }
 
-    public void button(){
+    public void submitButton() {
         WebElement submitButton = webDriver.findElement(submitTestBox);
         submitButton.click();
     }
@@ -70,5 +68,11 @@ public class TestBoxPage {
 
     public String getResultPermanentAddress() {
         return webDriver.findElement(findPermanentAddressByLocatorTestBox).getText();
+    }
+
+    public boolean emailHasError(){
+        String classes = webDriver.findElement(userEmail)
+                .getAttribute("class");
+        return classes.contains("field-error");
     }
 }
